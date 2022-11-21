9ad8b9ce642e5513c5d4830eac2d2bc53c903903
Angel Cruz <cruzangel99@gmail.com>
2022-11-20 19:16:54 -0500

    Made sure to move the secret key and tweaked the database location. Will be doing igrations within the next save

--- products_project/products_project/settings.py
+++ products_project/products_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b8s8@t38=xmlva8@!c40yd1^%_8)&m_ljr&(ik$ui#zu34y@90'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -68,20 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'products_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'products_database',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'Harmony0211!'
-    }
-}
 
 
 # Password validation
