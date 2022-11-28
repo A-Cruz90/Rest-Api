ad591d5d87a354444395d84e93ff57b07596c709
Angel Cruz <cruzangel99@gmail.com>
2022-11-28 16:40:30 -0500

    Started to generate code for post man collections. Generated some coding regarding each http requests. Currently testing endpoints to make sure they work properly.

--- products/views.py
+++ products/views.py
@@ -9,8 +9,33 @@ from .models import Product
 
 # Create your views here.
 
-@api_view(['GET','PUT','POST','DELETE'])
-def product_list(request):
-    product = Product.objects.all()
-    serializer = ProductSerializer(product, many = True)
-    return Response(serializer.data)
+@api_view(['GET','POST',])
+def product_list(request, ):
+    
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many =True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET','PUT','DELETE'])
+def product_detail(request,pk):
+    product = get_object_or_404(Product, pk = pk  )
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data = request.data )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
