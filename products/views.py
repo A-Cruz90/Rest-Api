05bca41b176cbe54bfd97d30ee9f29ed34184ca6
Angel Cruz <cruzangel99@gmail.com>
2022-11-28 15:48:10 -0500

    Ran into issue with enpoints. Updated my files within the views folder + updated my seriliazers.

--- products/views.py
+++ products/views.py
@@ -1,10 +1,16 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
+
+
 
 # Create your views here.
 
 @api_view(['GET','PUT','POST','DELETE'])
 def product_list(request):
-
-
-    return Response('Bet')
+    product = Product.objects.all()
+    serializer = ProductSerializer(product, many = True)
+    return Response(serializer.data)
